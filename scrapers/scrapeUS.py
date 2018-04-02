6e3dc8d7dac11cf8dc517dccd13fec461890da02
catherine lee <clee200@ucsc.edu>
2018-04-01 22:35:25 -0700

    lol cant believe i'm still doing this project...

---
+++ scrapers/scrapeUS.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+
+url = 'http://hosted.ap.org/dynamic/fronts/POLITICS?SITE=KSNEW&SECTION=HOME'
+html = urlopen(url)
+
+soup = BeautifulSoup(html, "html5lib")
+table = soup.find('tr', attrs = {'class': 'ap-topheadline-tr'}) #all inmate names are under <tbody>
+
+#print(table)
+row_info = []
+for row in table.findAll('p'):
+    print(row.text)
+
+
+
+ 
+
